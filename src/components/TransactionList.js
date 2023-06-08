13d45dae1889415c82a4170700e3bc0f8487fa50
PCYP <Chanyoungpark18@gmail.com>
2023-06-08 12:42:09 -0400

    Final configurations for the money format

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Transaction } from "./Transaction";
+
+import { GlobalContext } from "../context/GlobalState";
 
 export const TransactionList = () => {
-  return <div>TransactionList</div>;
+  const { transactions } = useContext(GlobalContext);
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+      </ul>
+    </>
+  );
 };
